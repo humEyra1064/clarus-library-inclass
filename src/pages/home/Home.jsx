1c88167302860ceede0ab79392284a23781dad99
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-03-10 10:26:27 +0300

    card styled

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/header/Header'
 import { HomeContainer, HomeImg, MainContainer } from './Home.style'
 import homeImg from "../../assets/books.jpg";
 import axios from "axios";
+import Card from '../../components/card/Card';
 
 const Home = () => {
   const [query, setQuery] = useState("");
@@ -38,9 +39,9 @@ const Home = () => {
         </HomeImg>
       ) : (
         <MainContainer wrap="wrap">
-          {/* {
+          {
             myData.map((item)=> <Card key={item.id} item={item} />)
-          } */}
+          }
         </MainContainer>
       )}
     </HomeContainer>
